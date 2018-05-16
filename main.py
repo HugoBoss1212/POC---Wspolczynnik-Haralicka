566699b20cf8b16a4c85db88826aa9230cb17dc5
HugoBoss1212 <lechowicz2@gmail.com>
2018-05-16 16:39:37 +0200

    dodanie funkcji obliczającej dystanse, dodanie funkcji obliczające współczynnik haralicka

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from skimage import io
 from skimage.color import rgb2gray
 import matplotlib.pyplot as plt
+import math
 
 
 def get_contour_points(image_):
@@ -20,6 +21,26 @@ def get_contour_points(image_):
     return points_
 
 
+def get_distances(points_, weight_point_):
+    distances_ = []
+
+    for point in points_:
+        distances_.append(math.sqrt(math.fabs(
+            ((point[0] - weight_point_[0]) ^ 2) + ((point[1] - weight_point_[1]) ^ 2))))
+    return distances_
+
+
+def get_haralick(distances_, length_):
+    sum1 = 0
+    sum2 = 0
+
+    for distance in distances_:
+        sum1 += distance
+        sum2 += math.pow(distance, 2)
+
+    return math.sqrt((math.pow(sum1, 2)) / (length_ * sum2 - 1))
+
+
 image = io.imread("test.png")
 image = rgb2gray(image)
 image = image < 1
@@ -27,8 +48,6 @@ image = image < 1
 points = get_contour_points(image)
 weight_point = (int((max(points, key=lambda t: t[0])[0] - min(points, key=lambda t: t[0])[0]) / 2),
                 int((max(points, key=lambda t: t[1])[1] - min(points, key=lambda t: t[1])[1]) / 2))
-
-
-plt.imshow(image, cmap="gray")
-plt.axis("off")
-plt.show()
+distances = get_distances(points, weight_point)
+haralick = get_haralick(distances, len(points))
+print("Współczynnik Haralicka: " + str(haralick))
